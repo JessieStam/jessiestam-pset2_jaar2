bddffb406dd7ca68b9c4dbe1b6fba7a67aee6eab
JessieStam <jessiestam@quicknet.nl>
2016-09-13 11:15:31 +0200

    Initial commit

---
+++ app/src/main/java/jstam/jessiestam_pset2_jaar2/MainActivity.java
@@ -0,0 +1,13 @@
+package jstam.jessiestam_pset2_jaar2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
