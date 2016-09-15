ed1ba1c4e8dfb0ffeb933742214b7204a3665951
JessieStam <jessiestam@quicknet.nl>
2016-09-15 14:46:18 +0200

    All activities - I don't get the hint to show

--- app/src/main/java/jstam/jessiestam_pset2_jaar2/SecondActivity.java
+++ app/src/main/java/jstam/jessiestam_pset2_jaar2/SecondActivity.java
@@ -2,6 +2,7 @@ package jstam.jessiestam_pset2_jaar2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -31,6 +32,7 @@ public class SecondActivity extends MainActivity {
     String new_words_left_text;
     String new_word_kind_text;
     Integer totalPlaceholdersLeft;
+    String nextPlaceholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,20 +46,13 @@ public class SecondActivity extends MainActivity {
         InputStream madlib3 = getResources().openRawResource(R.raw.madlib3_clothes);
         InputStream madlib4 = getResources().openRawResource(R.raw.madlib4_dance);
 
-
-        // construct a new story
-        //story = new Story(madlib0);
+        user_input = new EditText(this);
 
         // get a random integer between 0 and 4
         Random randomStoryNum = new Random();
         int storyNum = randomStoryNum.nextInt(5);
 
-
         // define story at random using random integer
-
-        // String storyTitle = "madlib" + storyNum;
-        //storyName = new Story(storyTitle);
-
         switch(storyNum) {
             case (0):
                 story = new Story(madlib0);
@@ -83,84 +78,72 @@ public class SecondActivity extends MainActivity {
 
         // REMEMBER storyNum zodat je niet meteen hetzelfde verhaaltje krijgt
 
-        // get total amount of placeholders and print to screen
-        //Integer totalPlaceholders = story.getPlaceholderCount();
+        // get total amount of placeholders and print to screen with instructions
         totalPlaceholdersLeft = story.getPlaceholderRemainingCount();
-
         wordsLeft = (TextView) findViewById(R.id.amount_left);
-        //wordsLeft.append(totalPlaceholders.toString() + " ");
-
-        //textview.setText(" append string" + textView.getText());
-
         words_left = totalPlaceholdersLeft.toString();
         wordsLeftInstruction = " word(s) left";
         wordsLeft.append(words_left + wordsLeftInstruction);
 
-        // get word kind and print to screen
-        String nextPlaceholder = story.getNextPlaceholder();
-
+        // get placeholder and print to screen with instructions (deze gaat nog niet goed, print eentje dubbel VRAAG)
+        nextPlaceholder = story.getNextPlaceholder();
         wordKind = (TextView) findViewById(R.id.word_kind);
         wordInstruction = "Please enter a/an ";
         wordKind.append(wordInstruction + nextPlaceholder.toLowerCase());
 
+        user_input = (EditText) findViewById(R.id.user_input_word);
+        user_input.setHint(nextPlaceholder);
+
+        // create list to remember words for when activity is killed
         filled_in_list = new ArrayList<>();
+
     }
 
     public void addWord(View addWordView) {
 
-        // change wordcount
-        // get total amount of placeholders and print to screen
+        // get total amount of placeholders and print to screen with instructions
         Integer totalPlaceholdersLeft = story.getPlaceholderRemainingCount() - 1;
-
-        //wordsLeft = (TextView) findViewById(R.id.amount_left); (volgens mij is dit extra)
-
-
         words_left = totalPlaceholdersLeft.toString();
         new_words_left_text = words_left + wordsLeftInstruction;
         wordsLeft.setText(new_words_left_text);
 
-        // change inputword
-        // get word kind and print to screen
+        // get placeholder and print to screen with instructions
         String nextPlaceholder = story.getNextPlaceholder();
-
-        // wordKind = (TextView) findViewById(R.id.word_kind); (ook extra)
         new_word_kind_text = wordInstruction + nextPlaceholder.toLowerCase();
         wordKind.setText(new_word_kind_text);
 
-        // save word from inputtext to string (en naar list voor einde activity)
+        // save word from EditText to string
         user_input = (EditText) findViewById(R.id.user_input_word);
         user_input_string = user_input.getText().toString();
 
+        // set EditText hint to placeholder
+        user_input.setHint(nextPlaceholder);
+
+        // save word to list for when activity is killed
         filled_in_list.add(user_input_string);
 
-        // input words (functie in story)
+        // input word in story
         story.fillInPlaceholder(user_input_string);
 
         //clear EditText
         user_input.getText().clear();
 
-        // if isFilledIn returns true -> next activity
-        Boolean filledIn = story.isFilledIn();
+        // create boolean to check if isFilledIn returns true
+        boolean filledIn = story.isFilledIn();
 
         // when everything is filled in, move on to third activity to print story
         if (filledIn) {
-            // create final story
+            // create final story String
             String finalStory = story.toString();
 
             // open third activity to display story
             Intent printStory = new Intent(this, ThirdActivity.class);
-
             printStory.putExtra("finalStory", finalStory);
 
-            // start new activity with the story
             startActivity(printStory);
 
-            // finish this activity
-            finish();
+            // finish current activity
         }
-
-
     }
-
     // onsavedinstacesate
 }
